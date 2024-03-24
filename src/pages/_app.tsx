d0a7edbb4e9f05e0256974c1f637fda9bc75a529
Pete Hodgson <git@thepete.net>
2024-03-24 11:19:27 -0700

    setup basic weather app (w. MUI)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,10 @@
-import "@/styles/globals.css";
+import { AppCacheProvider } from "@mui/material-nextjs/v13-pagesRouter";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <AppCacheProvider {...pageProps}>
+      <Component {...pageProps} />;
+    </AppCacheProvider>
+  )
 }
